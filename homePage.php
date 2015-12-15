a75c7f683f40689f84df29d94ca8046c79a432e1
avanics <avanimd@gmail.com>
2015-12-14 23:18:18 -0500

    Added changes for homepage
    Added changes for homepage

---
+++ homePage.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Neighbour Space</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/arial.js"></script>
+<script type="text/javascript" src="js/cuf_run.js"></script>
+</head>
+<?php
+$_SESSION['username'] = 'adm';
+include "connectdb.php";
+if(empty($_SESSION)) // if the session not yet started 
+   session_start();
+
+if(!isset($_SESSION['username'])) { //if not yet logged in
+   header("Location: login.php");// send to login page
+   exit;
+} 
+?>
+<body>
+<div class="main">
+  <div class="main_resize">
+    <div class="header">
+      <div class="logo">
+        <h1><a href="#"><span>Neighbour </span>  Space<small>  Share & Care</small></a></h1>
+      </div>
+      <div class="search">
+        <form method="get" id="search" action="#">
+          <span>
+          <input type="text" value="Search..." name="s" id="s" />
+          <input name="searchsubmit" type="image" src="images/search.gif" value="Go" id="searchsubmit" class="btn"  />
+          </span>
+        </form>
+        <!--/searchform -->
+        <div class="clr"></div>
+      </div>
+	  <div class = "message">
+		  <b>Welcome  <?php echo $_SESSION['username']; ?></b>
+		  </div>
+      <div class="menu_nav">
+
+        <ul>
+		
+          <li><a href="index.html">Profile</a></li>
+		  <li> <a href="logout.php">Logout</a></li>
+        </ul>
+		
+        <div class="clr"></div>
+      </div>
+	  
+      <div class="hbg"><img src="images/header_images.jpg" width="923" height="291" alt="" /></div>
+    </div>
+    <div class="content">
+      <div class="content_bg">
+        <div class="mainbar">
+        </div>
+        <div class="sidebar">
+          <div class="gadget">
+            <h2 class="star"><span>Sidebar</span> Menu</h2>
+            <div class="clr"></div>
+            <ul class="sb_menu">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Friends</a></li>
+              <li><a href="#">Neighbours</a></li>
+              <li><a href="#">Pending Friend Requests</a></li>
+              <li><a href="#">Block Requests</a></li>
+              <li><a href="#">Web Templates</a></li>
+            </ul>
+          </div>
+        
+        
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<div class="footer">
+  <div class="footer_resize">
+    <p class="lf">&copy; Copyright <a href="#">MyWebSite</a>.</p>
+    <p class="rf">Layout by Rocket <a href="http://www.rocketwebsitetemplates.com/">Website Templates</a></p>
+    <div class="clr"></div>
+  </div>
+</div>
+
+
+
+
+
+</body>
+
+</html>
