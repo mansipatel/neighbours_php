2e277e906c66dec40479d37f03dc8d2bb4ebbaf2
avanics <avanimd@gmail.com>
2015-12-21 09:40:02 -0500

    Adding Stored proc changes
    Adding Stored proc changes

--- friend_req.php
+++ friend_req.php
@@ -82,11 +82,11 @@ if(!isset($_SESSION['username'])) { //if not yet logged in
  
 
 <?php
-
+$sender_id = 0;
 echo '<form method  ="post">';
 $requests = "";
 if ($stmt = $mysqli->prepare("select u.first_name , u.last_name , n.hood_address , b.block_address , fr.sender_id  from neighbours.users u , neighbours.friend_requests fr , neighbours.neighbourhoods  n , neighbours.blocks b 
-where fr.sender_id = u.id  and u.hood_id = n.id and u.block_id = b.id and fr.user_id = '6';")) {
+where fr.sender_id = u.id  and u.hood_id = n.id and u.block_id = b.id and fr.user_id = '8' and fr.status = 'pending';")) {
   $stmt->execute();
   $stmt->bind_result( $first_name , $last_name , $hood_address , $block_address , $sender_id);
   if($stmt != null)
@@ -94,11 +94,7 @@ where fr.sender_id = u.id  and u.hood_id = n.id and u.block_id = b.id and fr.use
 	 echo '</br>';
 	 echo '</br>';
 		 
-	echo '<hd>';
 	
-	echo "You have pending friend requests";
-	
-	echo '</hd>';
 	echo '</br>';
 	echo '</br>';
 	echo '</br>';
@@ -106,9 +102,12 @@ where fr.sender_id = u.id  and u.hood_id = n.id and u.block_id = b.id and fr.use
 	echo "<div class='table-style-three'><table>
 <thead><tr><th>Name</th><th>Profile</th><th>Neighbourhood</th><th>Block</th><th>Accept</th><th>Reject</th></tr></thead>";
       while($stmt->fetch()) {
+			echo '<hd>';
+			echo "You have pending friend requests";
+			echo '</hd>';
 echo "<tr><td> $first_name  $last_name</td><td>Profile Pic</td><td> $block_address</td><td> $hood_address</td>
-<td><input type='submit' class= 'btn' name = 'Accept' value='Accept' id = 'accept' onClick = 'return acceptFriendReq($sender_id);'/></td>
-<td><input type='submit'  class = 'btn' name = 'Reject' value='Reject' id = 'reject' onClick = 'rejFriendReq($sender_id)'/></td>
+<td><input type='submit' class= 'btn' name = 'Accept' value='Accept' id = 'accept.$sender_id' onClick = 'return acceptFriendReq($sender_id);'/></td>
+<td><input type='submit'  class = 'btn' name = 'Reject' value='Reject' id = 'reject.$sender_id' onClick = 'rejFriendReq($sender_id)'/></td>
 </tr>
 ";
 
@@ -124,22 +123,27 @@ echo "<tr><td> $first_name  $last_name</td><td>Profile Pic</td><td> $block_addre
 	echo '</hd>';
 	}
   $stmt->close();
-  $mysqli->close();
+ 
 }
 else
 {
 	echo"There is some sql error encountered";
 }
-echo '<script>
-document.getElementById("accept").addEventListener("click", function(event){
-    event.preventDefault();
-});</script>';
-function acceptFriendReq($id)
-{
-	echo '<script> alert("Success"); </script>';
-
-$q=$cn->exec('CALL accept_friend_request("6" , $id , "accepted" ,@return_bit);');
-echo '<script> alert("Success"); </script>';
+if(isset($_POST['Accept']))
+{ 
+		echo $sender_id;
+		$result=$mysqli->prepare('CALL neighbours.accept_friend_request("6" , ? , "accepted" ,@return_bit)'); 
+		$result->bind_param("i", $sender_id);
+		$result->execute();
+		echo '<script> alert("Success"); </script>';
+}
+if(isset($_POST['Reject']))
+{ 
+		echo $sender_id;
+		$result=$mysqli->prepare('CALL neighbours.decline_friend_request("6" , ? , "rejected" ,@return_bit)'); 
+		$result->bind_param("i", $sender_id);
+		$result->execute();
+		echo '<script> alert("Success"); </script>';
 }
 echo '</form>';
 ?>
